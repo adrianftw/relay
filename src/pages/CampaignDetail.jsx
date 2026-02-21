82d06849edba521063e88b10fcd72d71cc4d9fdc
Adrian Thomas <adrianthomas@Adrians-MacBook-Pro.local>
2026-02-20 20:13:43 -0500

    Adjust map zoom level to 8

--- src/pages/CampaignDetail.jsx
+++ src/pages/CampaignDetail.jsx
@@ -39,7 +39,7 @@ export const CampaignDetail = () => {
     setMapViewState({
       longitude: lng,
       latitude: lat,
-      zoom: 7, // Zoom in to city level
+      zoom: 8, // Zoom in to city level
       transitionDuration: 1000 // Smooth 1-second transition
     });
   };
